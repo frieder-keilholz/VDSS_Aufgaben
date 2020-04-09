01b6afb4e2918cd504d3a0f51f1cd788645d90f0
frieder-keilholz <fgk1999@outlook.de>
2020-04-09 10:58:21 +0200

    get all keys

--- Aufgabenblatt2/todo.py
+++ Aufgabenblatt2/todo.py
@@ -5,3 +5,12 @@ r = redis.Redis(host='192.168.2.168',port=6379,db=0)
 print (r.get('Julia'))
 r.set('Oki', 'Doki')
 print (r.get('Oki'))
+print (r.keys())
+
+
+def getToDos():
+    rdbKeys = r.keys()
+    for key in rdbKeys:
+        print(key , ": ",r.get(key))
+
+getToDos()
