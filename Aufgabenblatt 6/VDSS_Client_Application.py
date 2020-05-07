901617ee40b18f910c4bb1c8a8ece674f4bbf996
Fury Capricorn <maxi.hartmann98@hotmail.de>
2020-05-07 19:35:34 +0200

    Added a Roadmap comment

--- Aufgabenblatt 6/VDSS_Client_Application.py
+++ Aufgabenblatt 6/VDSS_Client_Application.py
@@ -1,1 +1,16 @@
-print("hello world")
+"""
+ToDo List fot the client program
+	
+- Make simple Console UI
+- Make documents searchable by _id
+--	Make documents searcheable by everything
+---		Make documents searchable by 2 things at the same time (_id  and user)
+---		Make documents serchable by x number of searchterms
+--	Add stuff to the revcived document
+
+
+"""
+
+import pymongo
+
+print("hello")
