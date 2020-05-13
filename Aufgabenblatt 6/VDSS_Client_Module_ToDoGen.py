53bdef72465997ecd049228afe3cc938bde2e16c
frieder-keilholz <fgk1999@outlook.de>
2020-05-13 23:29:06 +0200

    added random ToDo generation code

--- Aufgabenblatt 6/VDSS_Client_Module_ToDoGen.py
+++ Aufgabenblatt 6/VDSS_Client_Module_ToDoGen.py
@@ -1,7 +1,21 @@
+from faker import Faker
+fake = Faker()
+
+import random
+import datetime
+import json
+
 # by user input
 def createToDo(dict):
     print("Create ToDo-JSON-Document by user-given values")
-    
+
 # generate random ToDo
 def generateToDo():
     print("Generate Random ToDo-JSON-Document")
+    entry = {}
+    entry["todo"] = fake.sentence()
+    entry["text"] = fake.text()
+    entry["until"] = fake.future_datetime(end_date='+10y')
+    entry["user"] = user
+    return json.dumps(entry,default=str,indent=4)
+    
