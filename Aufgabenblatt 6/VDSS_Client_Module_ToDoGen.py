c2df2fdf609c92b77a67fbf1a90ec2ef6b5dbf60
frieder-keilholz <fgk1999@outlook.de>
2020-05-13 22:49:08 +0200

    added todo-doc generator python file

---
+++ Aufgabenblatt 6/VDSS_Client_Module_ToDoGen.py
@@ -0,0 +1,7 @@
+# by user input
+def createToDo(dict):
+    print("Create ToDo-JSON-Document by user-given values")
+    
+# generate random ToDo
+def generateToDo():
+    print("Generate Random ToDo-JSON-Document")
